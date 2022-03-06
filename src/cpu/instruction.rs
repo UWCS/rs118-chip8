4cdf007f22038e4d37e9b0fa2cd25191b2f5afb0
Joe Harrison <josephharrison1@me.com>
2022-03-06 16:27:26 +0000

    add more instructions, flesh out CPU a bit more

--- src/cpu/instruction.rs
+++ src/cpu/instruction.rs
@@ -1,12 +1,33 @@
+use super::{eightbit, nibbles, twelvebit};
+
 pub enum Instruction {
-    Nop,        // 0nnn, sys instruction on original machines but not used anymore
-    Cls,        //00E0, clear display
-    Ret,        //00EE, return from a subroutine
-    Jmp(u16),   //1nnn, jump to address nnn
-    Ld(u8, u8), //6xkk, put value kk in register x
+    Nop,              // 0nnn, sys instruction on original machines but not used anymore
+    Cls,              //00E0, clear display
+    Ret,              //00EE, return from a subroutine
+    Jmp(u16),         //1nnn, jump to address nnn
+    Call(u16),        //2nnn, call subroutine at nnn
+    Ldr(u8, u8),      //6xkk, put value kk in register x
+    Add(u8, u8),      //7xkk, Adds the value kk to the value of register Vx
+    Ldi(u16),         //Annn, load value nnn into index register
+    Draw(u8, u8, u8), //Dxyn, display n byte-sprite starting at memory location I at coordinate (vx,vy), set VF = collision?
 }
 
 use Instruction::*;
 pub fn decode(opcode: u16) -> Instruction {
-    Nop
+    //the variable length operands
+    //see http://devernay.free.fr/hacks/chip8/C8TECH10.HTM
+    let nnn = twelvebit(opcode);
+    let kk = eightbit(opcode);
+    match nibbles(opcode) {
+        (0, 0, 0xE, 0xE) => Ret,
+        (0, 0, 0xE, 0) => Cls,
+        (0, _, _, _) => Nop,
+        (1, _, _, _) => Jmp(nnn),
+        (2, _, _, _) => Call(nnn),
+        (6, x, _, _) => Ldr(x, kk),
+        (7, x, _, _) => Add(x, kk),
+        (0xA, _, _, _) => Ldi(nnn),
+        (0xD, x, y, n) => Draw(x, y, n),
+        _ => unimplemented!(),
+    }
 }
--- src/cpu/mod.rs
+++ src/cpu/mod.rs
@@ -1,14 +1,17 @@
 mod font;
 mod instruction;
 mod test;
-struct CPU {
+
+use instruction::{decode, Instruction};
+
+pub struct CPU {
     memory: [u8; 4096],
     pc: u16,
+    index: u16,
     stack: Vec<u16>,
     delay: u8,
     sound: u8,
     registers: [u8; 16],
-    clock_speed: u32,
 }
 
 pub enum Interrupt {
@@ -16,7 +19,7 @@ pub enum Interrupt {
 }
 
 impl CPU {
-    fn init(&mut self, speed: u32) -> Self {
+    pub fn init() -> Self {
         let mut memory = [0_u8; 4096];
         //font is 80 bytes, should lie at 0x50
         memory[0x50..0xA0].copy_from_slice(&font::FONT);
@@ -24,16 +27,15 @@ impl CPU {
         CPU {
             memory: [0; 4096],
             pc: 0,
+            index: 0,
             delay: 0,
             sound: 0,
             stack: Vec::new(),
             registers: [0; 16],
-            clock_speed: speed,
         }
-        //load font and other defaults into memory
     }
 
-    pub fn fetch(&mut self) -> u16 {
+    fn fetch(&mut self) -> u16 {
         let mut instruction: u16 = 0;
         instruction &= (self.memory[self.pc as usize] as u16) << 8;
         instruction &= self.memory[(self.pc + 1) as usize] as u16;
@@ -41,11 +43,15 @@ impl CPU {
         instruction
     }
 
-    pub fn exectute(&mut self, instruction: u16) -> Option<Interrupt> {
+    fn exectute(&mut self, instruction: Instruction) -> Option<Interrupt> {
         None
     }
 
-    pub fn step(&mut self) {}
+    pub fn step(&mut self) -> Option<Interrupt> {
+        let opcode = self.fetch();
+        let instruction = decode(opcode);
+        self.exectute(instruction)
+    }
 }
 //helpers here
 
@@ -53,3 +59,24 @@ impl CPU {
 fn clear_screen() -> Interrupt {
     Interrupt::DisplayUpdate(Vec::new())
 }
+
+//break a u16 into its nibbles
+fn nibbles(n: u16) -> (u8, u8, u8, u8) {
+    let n3 = (n >> 12) as u8;
+    let n2 = ((n >> 8) & 0b1111) as u8;
+    let n1 = ((n >> 4) & 0b1111) as u8;
+    let n0 = (n & 0b1111) as u8;
+    (n3, n2, n1, n0)
+}
+
+//get the lower 12 bytes of a u16
+fn twelvebit(n: u16) -> u16 {
+    n & 0xfff
+}
+
+//get the lower 8 bytes of a u16
+fn eightbit(n: u16) -> u8 {
+    (n & 0xff) as u8
+}
+
+//helpers
