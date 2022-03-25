7bfa1961cce484f29f96344375a0192ef523310a
Joey <josephharrison1@me.com>
2022-03-25 19:42:11 +0000

    added clap cli

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,31 @@
 mod cpu;
 mod cpu_impl;
 
+use clap::Parser;
+
 fn main() {
+    let cli = Cli::parse();
+
+    let filename: &str = &cli.rom;
     let cpu = cpu_impl::Cpu::new(500)
-        .load("roms/chip8-test-rom-with-audio.ch8")
-        .expect("Could not load ROM");
+        .load(filename)
+        .unwrap_or_else(|_| panic!("Could not load ROM: {}", filename));
     cpu::Cpu::run(cpu);
+}
+
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+struct Cli {
+    /// A CHIP-8 ROM to load into the interpreter
+    #[clap(validator = rom_exists)]
+    rom: String,
+}
+
+fn rom_exists(f: &str) -> Result<(), &'static str> {
+    let p = std::path::Path::new(f);
+    if !p.is_file() {
+        Err("File does not exist.")
+    } else {
+        Ok(())
+    }
 }
