d6acc86b74bcc266ad8eee833ce1a0f56c4ba910
Joey <josephharrison1@me.com>
2022-03-26 16:20:32 +0000

    missed a doc comment

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 //!`chip8_base` provides everything you need to get started building your own CHIP-8 interpreter.
-//! See the documentation for the [`Cpu`][Cpu] trait to get started.
+//! See the documentation for the [`Interpreter`][Interpreter] trait to get started.
 
 mod interpreter;
 pub use {interpreter::Display, interpreter::Interpreter, interpreter::Keys};
